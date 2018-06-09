297248ba80df70f49f0b3816027c7f2d05041a0e
koobohyeon <goosee115@gmail.com>
2018-06-10 03:25:51 +0900

    add prop-types

--- src/Movie.js
+++ src/Movie.js
@@ -1,21 +1,30 @@
 import React, {Component} from 'react';
+import propTypes from 'prop-types'
 import './Movie.css';
 
 class Movie extends Component{
+  // 부모 컴포넌트에서 얻는 정보가 무엇인지, 있는지 없는지(isRequired)알 수 있다. 
+  static propTypes = {
+    title: propTypes.string.isRequired,
+    poster: propTypes.string.isRequired
+  }
   render() {
     return (
     <div>
       <h1>{this.props.title}</h1>
-      <MoviePoster img={this.props.poster}/>
+      <MoviePoster poster={this.props.poster}/>
     </div>
     )
   }
 }
 
 class MoviePoster extends Component{
+  static propTypes = {
+    poster: propTypes.string.isRequired
+  }
   render() {
     return (
-      <img src={this.props.img} />
+      <img src={this.props.poster} />
     )
   }
 }
